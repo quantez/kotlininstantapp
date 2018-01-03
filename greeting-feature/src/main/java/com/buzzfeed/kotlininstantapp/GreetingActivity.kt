4a1372a6a403f131aaedf8eb6ee076ef5c5fe3b0
Steve Peterson <steve.peterson@buzzfeed.com>
2018-01-02 22:21:20 -0600

    Create a feature module and move the old app logic to it.  Create a new app module that simply uses the feature module

--- greeting-feature/src/main/java/com/buzzfeed/kotlininstantapp/GreetingActivity.kt
+++ greeting-feature/src/main/java/com/buzzfeed/kotlininstantapp/GreetingActivity.kt
@@ -3,6 +3,7 @@ package com.buzzfeed.kotlininstantapp
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import com.buzzfeed.greetingfeature.R
 import com.buzzfeed.modellibrary.model.ActionModel
 import kotlinx.android.synthetic.main.activity_greeting.*
 
--- greeting-feature/src/main/java/com/buzzfeed/kotlininstantapp/GreetingActivityFragment.kt
+++ greeting-feature/src/main/java/com/buzzfeed/kotlininstantapp/GreetingActivityFragment.kt
@@ -1,10 +1,11 @@
 package com.buzzfeed.kotlininstantapp
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.buzzfeed.greetingfeature.R
 
 /**
  * A placeholder fragment containing a simple view.
